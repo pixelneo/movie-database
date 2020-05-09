e42ac9aadab8b66154d9c6d4b1bfb5137f541922
Ondřej Měkota <ondrej.mekota@me.com>
2020-05-09 18:13:04 +0200

    move from json to yaml

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import json
+import yaml
 
 class Config:
     def __init__(self, path):
         self._c = {}
-        with open(path, 'r') as f: 
-            obj = json.load(f)
+        with open(path, 'r') as f:
+            obj = yaml.safe_load(f)
             self.method = obj['method']
             self._c.update(obj['general'])
             self._c.update(obj['methods'][self.method])
@@ -20,5 +20,5 @@ class Config:
         return '\n'.join(['{}:  {}'.format(k,v) for k,v in self._c.items()])
 
 if __name__=='__main__':
-    c = Config('config.json')
+    c = Config('config.yaml')
     print(c)
